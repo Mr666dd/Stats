80b127c28001f10183a89d8cb5f8c036ece7328e
Mr.666dd <92013813+Mr666dd@users.noreply.github.com>
2022-02-13 12:22:43 +0500

    Add files via upload

---
+++ cmake/Stats.h
@@ -0,0 +1,13 @@
+﻿// Stats.h : включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта.
+
+#pragma once
+
+#include <iostream>
+#include <cstdio>
+#include <stdlib.h>
+#include <chrono>
+#include <thread>
+#include <Windows.h>
+
+// TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
